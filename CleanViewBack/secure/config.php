2d2e6e25cc436389109e581dd5524aec52e8b816
Taylor Love <planetman.hack@gmail.com>
2013-10-23 22:34:54 -0700

    Added a configuration file to contain database login information.

--- CleanViewBack/secure/config.php
+++ CleanViewBack/secure/config.php
@@ -1,3 +1,21 @@
 <?php
+#####################################################################
+# 									D O C U M E N T _ S U M M A R Y									#
+#####################################################################
+# The config will contain a number of constants to be used by the		#
+# rest of the software. 																						#
+#####################################################################
 
+#####################################################################
+# 						D A T A B A S E  _ S E T T I N G S		  							#
+#####################################################################
+define('DB_NAME','uwb_devdogs');
+define('DB_HOST','localhost');
+define('DB_USER','uwb');
+define('DB_PASS','Seattle2013');
+/* Example Use : 
+If I want to retrieve a constant 'DB_NAME', I can use any of the following methods to echo it into output.
+1) echo constant('DB_NAME');
+2) echo ${DB_NAME};
+3) echo DB_NAME; */
 ?>
