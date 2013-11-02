4fe3a5b8a596bf0ceadf43f34d41e899d02f534e
mgrzmiel <grzmiel@outlook.com>
2013-11-01 23:47:48 -0700

    Add missing API changes

--- CleanViewBack/api.php
+++ CleanViewBack/api.php
@@ -10,12 +10,18 @@
 	require_once('../config.php');           						#
 	require_once('secure/init_variables.php');						#
 	require_once('event.php');										#
+	require_once('school.php');										#
+	require_once('user.php');										#
+	require_once('course.php');										#
 #####################################################################
 //<editor-fold defaultstate="collapsed" desc="Api">
 class Api{
 	private static $tbl_Courses = 'courses';
 	private static $tbl_Enrollments = 'enrollments';
     private static $tbl_Events = 'events';
+    private static $tbl_Schools = 'schools';
+    private static $tbl_Users = 'users';
+    
 	
     public static $error_prefix = 'Error : ';
 	private $dbConn;
@@ -52,8 +58,75 @@ class Api{
 		}
 		return null;//if not found.
 	}
-	//</editor-fold>
+	function getSchoolById($schoolId) {
+		if (! isset ( $schoolId )) {
+			return null;
+		}
+		
+		$query = "SELECT * FROM " . Api::$tbl_Schools . " WHERE `school_id`=" . mysqli_real_escape_string ( $this->dbConn, $schoolId );
+		$mysqli_result = mysqli_query ( $this->dbConn, $query ) or die ( mysqli_error ( $this->dbConn ) );
+		$row = mysqli_fetch_row ( $mysqli_result ) or die ( mysqli_error ( $this->dbConn ) );
+		if (! isset ( $row )) {
+			return null;
+		} else {
+			return School::createFromTableRow ( $row );
+		}
+	}
+	function getCourseById($courseId) {
+		if (! isset ( $courseId )) {
+			return null;
+		}
+		
+		$query = "SELECT * FROM " . Api::$tbl_Courses . " WHERE `course_id`=" . mysqli_real_escape_string ( $this->dbConn, $courseId );
+		$mysqli_result = mysqli_query ( $this->dbConn, $query ) or die ( mysqli_error ( $this->dbConn ) );
+		$row = mysqli_fetch_row ( $mysqli_result ) or die ( mysqli_error ( $this->dbConn ) );
+		if (! isset ( $row )) {
+			return null;
+		} else {
+			return Course::createFromTableRow ( $row );
+		}
+	}
+	function getUserById($userId) {
+		if (! isset ( $userId )) {
+			return null;
+		}
+		
+		$query = "SELECT * FROM " . Api::$tbl_Users . " WHERE `user_id`=" . mysqli_real_escape_string ( $this->dbConn, $userId );
+		$mysqli_result = mysqli_query ( $this->dbConn, $query ) or die ( mysqli_error ( $this->dbConn ) );
+		$row = mysqli_fetch_row ( $mysqli_arr ) or die ( mysqli_error ( $this->dbConn ) );
+		if (! isset ( $row )) {
+			return null;
+		} else {
+			return User::createFromTableRow ( $row );
+		}
+	}
+	function getUsers() {
+		$query = "SELECT * FROM " . Api::$tbl_Users;
+		$mysqli_result = mysqli_query ( $this->dbConn, $query ) or die ( mysqli_error ( $this->dbConn ) );
+		$usersList = array ();
+		while ( $row = mysqli_fetch_row ( $mysqli_result ) ) {
+			$usersList [] = User::createFromTableRow ( $row );
+		}
+		return $usersList;
+	}
+	function getCourses() {
+		$query = "SELECT * FROM " . Api::$tbl_Courses;
+		$mysqli_result = mysqli_query ( $this->dbConn, $query ) or die ( mysqli_error ( $this->dbConn ) );
+		$coursesList = array ();
+		while ( $row = mysqli_fetch_row ( $mysqli_result ) ) {
+			$coursesList [] = Course::createFromTableRow ( $row );
+		}
+		return $coursesList;
+	}
+	function getSchools() {
+		$query = "SELECT * FROM " . Api::$tbl_Schools;
+		$mysqli_result = mysqli_query ( $this->dbConn, $query ) or die ( mysqli_error ( $this->dbConn ) );
+		$schoolsList = array ();
+		while ( $row = mysqli_fetch_row ( $mysqli_result ) ) {
+			$schoolsList [] = School::createFromTableRow ( $row );
+		}
+		return $schoolsList;
+	}
 }
-//</editor-fold>
+// </editor-fold>
 ?>
->>>>>>> branch 'master' of https://github.com/Pangamma/CleanView_Back.git
