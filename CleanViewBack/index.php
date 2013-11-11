c7947d90d1726fefce1aaf122ea1af5751144e7b
Taylor Love <pro.TaylorALove@Gmail.com>
2013-11-10 20:24:38 -0800

    fixing index page

--- CleanViewBack/index.php
+++ CleanViewBack/index.php
@@ -1,23 +1,86 @@
+
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>test usage</title>
-    </head>
-    <body>
-		<?php
-		require_once('api.php');
-		$api = new Api();
-		$courses = $api->getCourses();
-		foreach($courses as $key => $val){
-			echo $val->getName().'<br/>';
-		}
-		
-//		$users = $api->getUsers();
-//		foreach($users as $user){
-//			echo $user->getUsername();
-//		}
-		
-		?>
-    </body>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Template &middot; Bootstrap</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles --> 
+    <link href="assets/stylesheets/style.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="../assets/ico/favicon.png">
+  </head>
+
+  <body>
+	
+	<div class="b-header">
+		<div class="g-container">
+			
+			<h1 class="b-header__logo">Logo</h1>
+				
+			<form class="b-login-form">
+				<input type="text" class="b-login-form__email" placeholder="Email">
+				<input type="password" class="b-login-form__password" placeholder="Password">
+				<button type="submit" class="b-login-form__button">Sign in</button>
+				<div>
+					<label class="b-login-form__rmbr-label">
+	    				<input class="b-login-form__rmbr-input" type="checkbox"> Remember me
+	 				</label>
+	 				<a class="b-login-form__forgot-password">Forgot your password?</a>
+	 			</div>	
+			</form>
+		</div>
+	</div>
+	
+    <div class="b-signup">
+		<div class="g-container">		
+	        <!-- Jumbotron -->
+	        <div class="b-signup__main">
+		        <h1 class="b-signup__logo">Logo and name</h1>
+		        <p class="b-signup__lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+		        <a class="b-signup__button" href="#">Sign up today</a>
+	    	</div>
+      	</div>
+
+    </div>
+	
+	<div class="b-main">
+      <!-- Example row of columns -->
+      	<div class="b-features-col">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+      	</div>
+        <div class="b-features-col">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+       	</div>
+        <div class="b-features-col">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
+        </div>
+      
+
+      <div class="footer g-span12">
+        <p>&copy; Company 2013</p>
+      </div>
+
+    </div> <!-- /b-main -->
+
+    <!--javascript-->
+    <script src="../assets/js/jquery.js"></script>
+
+  </body>
 </html>
